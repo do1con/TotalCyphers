431d5e9ed0756afdb10753f43c1919885fb44ae1
do1con <kss7547@gmail.com>
2020-11-21 02:33:05 +0900

    20201121_kss7547 1. react-router-dom 적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useCallback } from "react";
 import { addTest } from "./modules/totalCyphers";
 import { decreaseTestAsync } from "./modules/sagas";
 import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 import { RootState } from "./modules";
 import "antd/dist/antd.css";
 import Header from "./Layout/Header";
 import MainPageBody from "./Layout/MainPageBody";
 import Footer from "./Layout/Footer";
+import TestComp from "./inc/TestComp";
 
 function App(): JSX.Element {
   const test = useSelector((state: RootState) => state.totalCyphers.test);
@@ -24,10 +26,13 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <Header />
-      <MainPageBody />
-      {/* <p>스테이트 : {test}</p>
+      <BrowserRouter>
+        <Route path="/" component={MainPageBody} exact />
+        <Route path="/test" component={TestComp} exact />
+        {/* <p>스테이트 : {test}</p>
       <button onClick={onClickIncBtn}>테스트 액션트리거</button>
       <button onClick={onClickDecBtn}>테스트 사가액션 트리거</button> */}
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ front/src/inc/TestComp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComp = () => {
+  return (
+    <div>
+      <span>테스트</span>
+    </div>
+  );
+};
+
+export default TestComp;
