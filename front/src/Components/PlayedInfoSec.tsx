0a297b1718d24a851d7da53588abaf5a54f36a01
do1con <kss7547@gmail.com>
2020-11-23 06:33:31 +0900

    20201123_kss7547 1. 포지션 아이콘 추가 & 전적에 맞게 디스플레이 2. KDA 정보 표시

--- front/src/Components/PlayedInfoSec.tsx
+++ front/src/Components/PlayedInfoSec.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import tanker from "./../static/media/tanker.png";
+import suppoter from "./../static/media/supporter.png";
+import geun_dealer from "./../static/media/geun_dealer.png";
+import one_dealer from "./../static/media/one_dealer.png";
 
 function PlayedInfoSec(data: any) {
   const info = data.data;
@@ -16,7 +20,65 @@ function PlayedInfoSec(data: any) {
           alt="캐릭터"
         />
       </div>
-      어쩌구저쩌구
+      <div
+        style={{
+          overflow: "hidden",
+          margin: "10px",
+        }}
+      >
+        <img
+          src={
+            info.position.name === "탱커"
+              ? tanker
+              : info.position.name === "원거리딜러"
+              ? one_dealer
+              : info.position.name === "근거리딜러"
+              ? geun_dealer
+              : info.position.name === "서포터"
+              ? suppoter
+              : ""
+          }
+          alt="포지션"
+          width="25"
+        />
+      </div>
+      <div style={{ margin: "10px" }}>
+        {info.position.attribute.map((data: any, index: number) => (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              borderRadius: "25% 25%",
+              overflow: "hidden",
+            }}
+            key={index}
+          >
+            <img
+              src={`https://img-api.neople.co.kr/cy/position-attributes/${data.id}`}
+              alt="특성"
+              width="27"
+            />
+          </div>
+        ))}
+      </div>
+      <div style={{ margin: "10px", display: "flex", flexDirection: "column" }}>
+        <div>
+          <span
+            style={{ fontSize: "12px", color: "#6f6f6f", textAlign: "center" }}
+          >
+            K / D / A
+          </span>
+        </div>
+        <div>
+          <span style={{ fontWeight: "bold" }}>{info.playInfo.killCount}</span>{" "}
+          /{" "}
+          <span style={{ fontWeight: "bold" }}>{info.playInfo.deathCount}</span>{" "}
+          /{" "}
+          <span style={{ fontWeight: "bold" }}>
+            {info.playInfo.assistCount}
+          </span>
+        </div>
+      </div>
     </PlayedInfoWrapper>
   );
 }
@@ -28,4 +90,5 @@ const PlayedInfoWrapper = styled.div<{ win: boolean }>`
   background-color: ${(props) => (props.win ? "#bae7ff" : "#ffccc7")};
   display: flex;
   align-items: center;
+  margin-bottom: 10px;
 `;
