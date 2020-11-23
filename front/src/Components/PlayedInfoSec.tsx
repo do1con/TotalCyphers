809cd6e75e56b0d3e5c720d8306f5ec7a373fa89
do1con <kss7547@gmail.com>
2020-11-24 06:32:45 +0900

    20201124_kss7547 1. ant-design/icons 추가 (했는데 gitignore도 아닌데 왜 변경 감지 안되는지 모르겠음) 2. 매치 상세정보 보기 버튼 추가

--- front/src/Components/PlayedInfoSec.tsx
+++ front/src/Components/PlayedInfoSec.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useCallback } from "react";
 import styled from "styled-components";
+import { DownOutlined } from "@ant-design/icons";
 import tanker from "./../static/media/tanker.png";
 import suppoter from "./../static/media/supporter.png";
 import geun_dealer from "./../static/media/geun_dealer.png";
@@ -62,199 +63,227 @@ function PlayedInfoSec(data: any): JSX.Element {
     }
   }, [info.date]);
   return (
-    <PlayedInfoWrapper win={info.playInfo.result === "win"}>
-      <div
-        style={{
-          position: "relative",
-          zIndex: 2,
-          width: "65px",
-          textAlign: "center",
-        }}
-      >
-        <span>&nbsp;{getDateDiffer()}</span>
-      </div>
-      <div
-        style={{
-          height: "101px",
-          position: "absolute",
-          zIndex: 1,
-        }}
-      >
-        <span
+    <div style={{ height: "121px", marginBottom: "10px" }}>
+      <PlayedInfoWrapper win={info.playInfo.result === "win"}>
+        <div
           style={{
-            fontWeight: "bolder",
-            fontSize: "155px",
-            lineHeight: "0.5",
-            color: "#ffffff",
-            opacity: "0.5",
-            display: "block",
-            overflowY: "hidden",
-            overflowX: "visible",
+            position: "relative",
+            zIndex: 2,
+            width: "65px",
+            textAlign: "center",
+          }}
+        >
+          <span>&nbsp;{getDateDiffer()}</span>
+        </div>
+        <div
+          style={{
             height: "101px",
-          }}
-        >
-          {info.playInfo.result.toUpperCase()}
-        </span>
-      </div>
-      <div style={{ position: "relative", zIndex: 2 }}>
-        <div
-          style={{
-            borderRadius: "50% 50%",
-            overflow: "hidden",
-            margin: "10px",
-          }}
-        >
-          <img
-            src={`https://img-api.neople.co.kr/cy/characters/${info.playInfo.characterId}`}
-            alt="캐릭터"
-          />
-        </div>
-        <div style={{ width: "100%", textAlign: "center" }}>
-          <span style={{ fontSize: "14px", fontWeight: "bold" }}>
-            {info.playInfo.characterName}
-          </span>
-        </div>
-      </div>
-      <div
-        style={{
-          overflow: "hidden",
-          margin: "10px",
-          position: "relative",
-          zIndex: 2,
-        }}
-      >
-        <img
-          src={
-            info.position.name === "탱커"
-              ? tanker
-              : info.position.name === "원거리딜러"
-              ? one_dealer
-              : info.position.name === "근거리딜러"
-              ? geun_dealer
-              : info.position.name === "서포터"
-              ? suppoter
-              : "에러"
-          }
-          alt="포지션"
-          width="25"
-        />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "80px",
-          position: "relative",
-          zIndex: 2,
-        }}
-      >
-        <div style={{ textAlign: "center" }}>
-          <span style={{ fontSize: "12px", color: "#6f6f6f" }}>K / D / A</span>
-        </div>
-        <div style={{ textAlign: "center" }}>
-          <span style={{ fontWeight: "bold" }}>{info.playInfo.killCount}</span>{" "}
-          /{" "}
-          <span style={{ fontWeight: "bold" }}>{info.playInfo.deathCount}</span>{" "}
-          /{" "}
-          <span style={{ fontWeight: "bold" }}>
-            {info.playInfo.assistCount}
-          </span>
-        </div>
-        <div style={{ textAlign: "center" }}>{kda()} 평점</div>
-      </div>
-      <div style={{ width: "80px", position: "relative", zIndex: 2 }}>
-        여기에 정보 ㅇ
-      </div>
-      <div style={{ margin: "10px", position: "relative", zIndex: 3 }}>
-        {info.position.attribute.map((data: any, index: number) => (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              overflow: "hidden",
-            }}
-            key={index}
-          >
-            <div>
-              <img
-                src={`https://img-api.neople.co.kr/cy/position-attributes/${data.id}`}
-                alt="특성"
-                width="27"
-                style={{
-                  borderRadius: "25% 25%",
-                }}
-              />
-              <span style={{ fontSize: "12px" }}>&nbsp;{data.name}</span>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          zIndex: 2,
-          marginLeft: "auto",
-          right: "0",
-        }}
-      >
-        <img
-          src={
-            info.map.mapId === "101"
-              ? map_riverford
-              : info.map.mapId === "102"
-              ? map_metropolis
-              : info.map.mapId === "103"
-              ? map_bristol
-              : info.map.mapId === "104"
-              ? map_springfield
-              : info.map.mapId === "105"
-              ? map_grandflam
-              : info.map.mapId === "106"
-              ? map_eindhoven
-              : ".."
-          }
-          alt="맵"
-          height="101"
-        />
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
             position: "absolute",
-            top: "0",
-            left: "0",
-            background: `linear-gradient(60deg, ${
-              info.playInfo.result === "win"
-                ? "rgba(186, 231, 255, 1)"
-                : "rgba(255, 204, 199, 1)"
-            }30%, rgba(255,255,255,0))`,
+            zIndex: 1,
           }}
         >
-          <div
+          <span
             style={{
-              position: "absolute",
-              top: "50px",
-              right: "0",
-              backgroundColor: "rgba(0, 0, 0, 0.8)",
-              width: "100px",
-              height: "30px",
-              textAlign: "center",
+              fontWeight: "bolder",
+              fontSize: "155px",
+              lineHeight: "0.5",
+              color: "#ffffff",
+              opacity: "0.5",
+              display: "block",
+              overflowY: "hidden",
+              overflowX: "visible",
+              height: "101px",
             }}
           >
-            <span
-              style={{
-                fontSize: "13px",
-                fontWeight: "bold",
-                color: "#ffffff",
-                lineHeight: "30px",
-              }}
-            >
-              {info.map.name}
+            {info.playInfo.result.toUpperCase()}
+          </span>
+        </div>
+        <div style={{ position: "relative", zIndex: 2 }}>
+          <div
+            style={{
+              borderRadius: "50% 50%",
+              overflow: "hidden",
+              margin: "10px",
+            }}
+          >
+            <img
+              src={`https://img-api.neople.co.kr/cy/characters/${info.playInfo.characterId}`}
+              alt="캐릭터"
+            />
+          </div>
+          <div style={{ width: "100%", textAlign: "center" }}>
+            <span style={{ fontSize: "14px", fontWeight: "bold" }}>
+              {info.playInfo.characterName}
             </span>
           </div>
         </div>
-      </div>
-    </PlayedInfoWrapper>
+        <div
+          style={{
+            overflow: "hidden",
+            margin: "10px",
+            position: "relative",
+            zIndex: 2,
+          }}
+        >
+          <img
+            src={
+              info.position.name === "탱커"
+                ? tanker
+                : info.position.name === "원거리딜러"
+                ? one_dealer
+                : info.position.name === "근거리딜러"
+                ? geun_dealer
+                : info.position.name === "서포터"
+                ? suppoter
+                : "에러"
+            }
+            alt="포지션"
+            width="25"
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "80px",
+            position: "relative",
+            zIndex: 2,
+          }}
+        >
+          <div style={{ textAlign: "center" }}>
+            <span style={{ fontSize: "12px", color: "#6f6f6f" }}>
+              K / D / A
+            </span>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <span style={{ fontWeight: "bold" }}>
+              {info.playInfo.killCount}
+            </span>{" "}
+            /{" "}
+            <span style={{ fontWeight: "bold" }}>
+              {info.playInfo.deathCount}
+            </span>{" "}
+            /{" "}
+            <span style={{ fontWeight: "bold" }}>
+              {info.playInfo.assistCount}
+            </span>
+          </div>
+          <div style={{ textAlign: "center" }}>{kda()} 평점</div>
+        </div>
+        <div
+          style={{
+            width: "80px",
+            position: "relative",
+            zIndex: 2,
+            textAlign: "center",
+          }}
+        >
+          <span style={{ fontSize: "12px", color: "#6f6f6f" }}>공격량</span>
+          <br />
+          <span style={{ color: "#00860a", fontWeight: "bold" }}>
+            {info.playInfo.attackPoint}
+          </span>
+          <br />
+          <span style={{ fontSize: "12px", color: "#6f6f6f" }}>피해량</span>
+          <br />
+          <span style={{ color: "#690000", fontWeight: "bold" }}>
+            {info.playInfo.damagePoint}
+          </span>
+        </div>
+        <div style={{ margin: "10px", position: "relative", zIndex: 3 }}>
+          {info.position.attribute.map((data: any, index: number) => (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                overflow: "hidden",
+              }}
+              key={index}
+            >
+              <div>
+                <img
+                  src={`https://img-api.neople.co.kr/cy/position-attributes/${data.id}`}
+                  alt="특성"
+                  width="27"
+                  style={{
+                    borderRadius: "25% 25%",
+                  }}
+                />
+                <span style={{ fontSize: "12px" }}>&nbsp;{data.name}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 2,
+            marginLeft: "auto",
+            right: "0",
+          }}
+        >
+          <img
+            src={
+              info.map.mapId === "101"
+                ? map_riverford
+                : info.map.mapId === "102"
+                ? map_metropolis
+                : info.map.mapId === "103"
+                ? map_bristol
+                : info.map.mapId === "104"
+                ? map_springfield
+                : info.map.mapId === "105"
+                ? map_grandflam
+                : info.map.mapId === "106"
+                ? map_eindhoven
+                : ".."
+            }
+            alt="맵"
+            height="101"
+          />
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              top: "0",
+              left: "0",
+              background: `linear-gradient(60deg, ${
+                info.playInfo.result === "win"
+                  ? "rgba(186, 231, 255, 1)"
+                  : "rgba(255, 204, 199, 1)"
+              }30%, rgba(255,255,255,0))`,
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: "50px",
+                right: "0",
+                backgroundColor: "rgba(0, 0, 0, 0.8)",
+                width: "100px",
+                height: "30px",
+                textAlign: "center",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "13px",
+                  fontWeight: "bold",
+                  color: "#ffffff",
+                  lineHeight: "30px",
+                }}
+              >
+                {info.map.name}
+              </span>
+            </div>
+          </div>
+        </div>
+      </PlayedInfoWrapper>
+      <PlayedInfoDetailBtn win={info.playInfo.result === "win"}>
+        <DownOutlined style={{ margin: "0 auto" }} />
+      </PlayedInfoDetailBtn>
+    </div>
   );
 }
 
@@ -265,7 +294,13 @@ const PlayedInfoWrapper = styled.div<{ win: boolean }>`
   background-color: ${(props) => (props.win ? "#bae7ff" : "#ffccc7")};
   display: flex;
   align-items: center;
-  margin-bottom: 10px;
   position: relative;
   height: 101px;
+`;
+const PlayedInfoDetailBtn = styled.div<{ win: boolean }>`
+  width: 100%;
+  height: 20px;
+  background-color ${(props) => (props.win ? "#91d5ff" : "#ffa39e")};
+  text-align: center;
+  cursor: pointer
 `;
